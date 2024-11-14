c6e391418343ff0e6b417b4f8a5e874cd6e1a8b2
annalandi1 <landianna.1106@gmail.com>
2024-11-14 16:24:47 +0100

    update

--- pexel.js
+++ pexel.js
@@ -1,4 +1,4 @@
-const img = "https://api.pexels.com/v1/search?query=nature"
+const imgQ = "https://api.pexels.com/v1/search?query=nature"
 const secImg = "https://api.pexels.com/v1/search?query=landscapes"
 
 const loadImg = document.getElementById("load-img")
@@ -7,7 +7,7 @@ const loadSecImg = document.getElementById("load-sec-img")
 loadImg.addEventListener("click", function(){
   
     console.log("ho caricato le immagini?")
-    fetch(img,{
+    fetch(imgQ,{
          method: "GET",
          headers:{
             "Content-Type": "application/json"
@@ -21,13 +21,49 @@ loadImg.addEventListener("click", function(){
          throw new Error("Errore nel recupero dei dati")
         }
      })
-    .then((images) => {
-        let imgArray= document.querySelectorAll("img")
-
-    }
-
-    )
+    .then((data) => {
+        console.log(data)
+        let images= document.getElementsByClassName("cart-img-top")
+          
+        const imgArr = Array.from(images)
+        for (let i = 0; i < imgArr.length; i++) {
+            imgArr[i].setAttribute("src", ` ${data.photos[i].src.medium}`)
+            }
+    })
+    .catch((error) => {
+        console.log("error", error)
+    })
 })
 
+loadSecImg.addEventListener("click", function(){
+  
+    console.log("ho caricato le immagini?")
+    fetch(secImg,{
+         method: "GET",
+         headers:{
+            "Content-Type": "application/json"
+             "Authorization": "KSFyYnaPgQBIXjoVAY2L1vPFUz7MEWfDcwVuyYMQsAKa501vYnSyuMmE"
+            }
+    })
+    .then((response) => {
+        if (response.ok) {
+            return response.json()
+        } else {
+         throw new Error("Errore nel recupero dei dati")
+        }
+     })
+     .then((data) => {
+        console.log(data)
+        let images= document.getElementsByClassName("cart-img-top")
+          
+        const imgArr = Array.from(images)
+        for (let i = 0; i < imgArr.length; i++) {
+            imgArr[i].setAttribute("src", ` ${data.photos[i].src.medium}`)
+            }
+    })
+    .catch((error) => {
+        console.log("error", error)
+    })
+})
 
 
